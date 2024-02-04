936d57f1cffebdbced30575e5f14df428ce6f261
mrvnzcn <imervanozcan@gmail.com>
2024-02-04 19:28:11 +0300

    form validations

--- Controllers/MeetingController.cs
+++ Controllers/MeetingController.cs
@@ -15,9 +15,14 @@ namespace MeetingApp.Controllers
         [HttpPost]
         public IActionResult Apply(UserInfo model)
         {
-            Repository.CreateUser(model);
+            if(ModelState.IsValid) {
+                Repository.CreateUser(model);
             ViewBag.UserCount = Repository.Users.Where(info=> info.WillAttend == true).Count();
             return View("Thanks", model);
+            } else {
+                return View(model);
+            }
+            
         }
 
         [HttpGet]
--- Models/UserInfo.cs
+++ Models/UserInfo.cs
@@ -1,11 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace MeetingApp.Models
 {
     public class UserInfo
     {
         public Guid Id { get; set; }
+        
+        [Required(ErrorMessage = "Ad Soyad alanı zorunludur.")]
         public string? Name { get; set; }
+        [Required(ErrorMessage = "Telefon alanı zorunludur.")]
         public string? Phone { get; set; }
+        [Required(ErrorMessage = "Email alanı zorunludur.")]
+        [EmailAddress(ErrorMessage = "Hatalı email girdiniz.")]
         public string? Email { get; set; }
+        
         public bool? WillAttend { get; set; }
     }
 }
