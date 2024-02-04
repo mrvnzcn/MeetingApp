0e30199b1146d5987b0d0bfaf60a2999eb7b3c2a
mrvnzcn <imervanozcan@gmail.com>
2024-02-04 16:38:49 +0300

    Model binding

--- Controllers/MeetingController.cs
+++ Controllers/MeetingController.cs
@@ -5,16 +5,29 @@ namespace MeetingApp.Controllers
    
     public class MeetingController : Controller
     {
+        [HttpGet]
         public IActionResult Index()
         {
             return View();
         }
 
+        [HttpGet]
         public IActionResult Apply()
         {
             return View();
         }
 
+        [HttpPost]
+        public IActionResult Apply(string Name, string Phone, string Email, bool WillAttend)
+        {
+            // Console.WriteLine(Name, Email, Phone, WillAttend);
+            // Console.WriteLine(Email);
+            // Console.WriteLine(Phone);
+            // Console.WriteLine(WillAttend);
+            return View();
+        }
+
+        [HttpGet]
         public IActionResult List()
         {
             return View();
